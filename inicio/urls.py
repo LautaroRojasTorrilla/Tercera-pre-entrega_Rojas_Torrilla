bb380e133528e927f7193e0d45aff78548391739
LautaroRojasTorrilla <lautaro.rojas.torrilla@gmail.com>
2023-04-10 22:03:11 -0300

    se realizo la herencia de template

--- inicio/urls.py
+++ inicio/urls.py
@@ -5,4 +5,5 @@ app_name = 'inicio'
 
 urlpatterns = [
     path('', views.mi_vista, name='inicio'),
+    path('crear-remera/', views.crear_remera, name='crear_remera'),
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render, redirect
 
 def mi_vista(request):
     return render(request, 'index.html')
+
+def crear_remera(request):
+    ...
