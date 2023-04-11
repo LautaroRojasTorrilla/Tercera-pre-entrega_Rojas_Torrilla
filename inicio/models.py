0c5f6a1cd0bac441ea7966184346d5f09e36041e
LautaroRojasTorrilla <lautaro.rojas.torrilla@gmail.com>
2023-04-10 22:23:51 -0300

    creacion de superusuario

--- inicio/models.py
+++ inicio/models.py
@@ -9,4 +9,18 @@ class Remera(models.Model):
     origen = models.CharField(max_length=20)
     
     def __str__(self):
-        return f'El modelo es {self.modelo} con el diseño {self.diseño}, talle: {self.talle}. Origen: {self.origen}.'
+        return f'El modelo es "{self.modelo}" con diseño de "{self.diseño}", talle: "{self.talle}". Origen: "{self.origen}".'
+    
+# class Remera(models.Model):
+#     modelo = models.CharField(max_length=20)
+#     talle = models.CharField(max_length=5)
+#     diseño = models.CharField(max_length=20)
+#     origen = models.CharField(max_length=20)
+
+#     def __str__(self):
+#         table = "| {:<10} | {:<5} | {:<20} | {:<20} |\n".format("Modelo", "Talle", "Diseño", "Origen")
+#         table += "| {:<10} | {:<5} | {:<20} | {:<20} |\n".format("-"*10, "-"*5, "-"*20, "-"*20)
+#         table += "| {:<10} | {:<5} | {:<20} | {:<20} |\n".format(self.modelo, self.talle, self.diseño, self.origen)
+#         table += "\n"
+#         return table
+#hice esta clase, pero no se veía correctamente en el admin. Supongo que es una limitacion de la visualizacion.
